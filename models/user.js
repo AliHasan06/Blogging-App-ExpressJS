0a048595d16b89f9572a4c9f31785229dbbad1be
Alihasan06 <alihassanraja06@gmail.com>
2024-12-24 10:12:28 -0800

    commited

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/data-association")
+
+const postSchema = new mongoose.Schema({
+    postdata: String,
+    user: String,
+    data:{
+        type: Date,
+        default: Date.now
+    }
+   
+});
+
+module.exports = mongoose.model('post', postSchema);
