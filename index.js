ee4ed8222fff112fbc016f6667e89e6ac0f632b4
Alihasan06 <alihassanraja06@gmail.com>
2024-12-25 15:58:48 -0800

    `Updated dependencies and removed unused models`

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require("express");
 const app = express();
 const usermodel = require("./models/user");
-const postmodel = require("./models/post");
+const cookieParser = require("cookie-parser");
+
+app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
-app.get("/create", async (req, res) => {
-  let user = await usermodel.create({
-    username: "ali",
-    age: 18,
-    email: "ali@example.com",
-  });
-  res.send(user);
-});
-app.get("/post/create", async (req, res) => {
-  let post = await postmodel.create({
-    postdata: "hello saare log kaise ho",
-    user: "676c4e3ef9f05a623cb96ffb",
-  });
-  let user = await usermodel.findOne({ _id: "676c4e3ef9f05a623cb96ffb" });
-  user.post.push(post._id);
-  await user.save();
-  res.send({post,user});
-});
 
 app.listen(3000, () => {});
--- models/user.js
+++ models/user.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject")
+
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   age: Number,
-  post: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "post",  
-    }
-  ],
+  password: String,
+  name: String
+ 
 });
 
 module.exports = mongoose.model("User", userSchema); 
